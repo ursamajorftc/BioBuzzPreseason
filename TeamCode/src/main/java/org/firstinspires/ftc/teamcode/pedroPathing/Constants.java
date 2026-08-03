ecaff6b45e4c778235a942478c6421ab3e45f5bd
Ursa-Major22774 <152826539+Ursa-Major22774@users.noreply.github.com>
2026-08-02 17:17:33 -0700

    8/2/26-p2
    Added configurable to allow panels to modify Heading variables. Additional testing needed to finalize Heading tuning.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
+import com.bylazar.configurables.annotations.Configurable;
 import com.pedropathing.control.PIDFCoefficients;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
@@ -13,6 +14,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
+@Configurable
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
             .mass(8.7)
@@ -35,7 +37,6 @@ public class Constants {
             .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE)
             .xVelocity(61.103)
             .yVelocity(45.523);
-
 
 
     public static PinpointConstants localizerConstants = new PinpointConstants()
